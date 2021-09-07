21e574b5521aea0431d279e708deaf5cbff3ad19
KateVatson <katevatson@icloud.com>
2021-09-07 23:17:34 +0300

    Поиск максимального элемента, лямбда выражения и добавление сортировки списков
    Лекции 4.8, 4.9, 4.10

--- addressbook-web-tests/src/test/java/ru/stqa/lat/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/lat/addressbook/tests/GroupCreationTest.java
@@ -1,9 +1,10 @@
 package ru.stqa.lat.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.lat.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase{
@@ -12,10 +13,18 @@ public class GroupCreationTest extends TestBase{
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+
+
+    before.add(group);
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
